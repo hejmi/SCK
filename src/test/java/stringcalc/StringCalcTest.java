c9710f7a7483ea7e53b27d6343dc84fe06e83044
MichMatch <michael@graphicdesire.se>
2020-12-04 13:35:53 +0100

    added StringCalc.java, first test OK

---
+++ src/test/java/stringcalc/StringCalcTest.java
@@ -0,0 +1,15 @@
+package stringcalc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringCalcTest {
+
+    @Test
+    public void checkIfClassExists() {
+        StringCalc calc = new StringCalc();
+    }
+
+
+}
